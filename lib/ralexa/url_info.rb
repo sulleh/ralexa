1928c29f34e427dacd05c22cccd8a4271c8241e5
Liam Norton <liam.norton@flippa.com>
2013-10-22 15:04:46 +1100

    Url info .get method to return a hash of useful alexa data

--- lib/ralexa/url_info.rb
+++ lib/ralexa/url_info.rb
@@ -1,19 +1,117 @@
 module Ralexa
   class UrlInfo < AbstractService
 
-    # The site's Alexa rank
+    # Alexa data for an individual site
+    def get(url, params = {})
+      result({"ResponseGroup" => "Related,TrafficData,ContentData", "Url" => url}, params) do |doc|
+        @document = doc
+
+        {
+          speed_median_load_time:       speed_median_load_time,
+          speed_load_percentile:        speed_load_percentile,
+          link_count:                   link_count,
+          ranking:                      ranking,
+          ranking_delta:                ranking_delta,
+          reach_rank:                   reach_rank,
+          reach_rank_delta:             reach_rank_delta,
+          reach_per_million:            reach_per_million,
+          reach_per_million_delta:      reach_per_million_delta,
+          page_views_rank:              page_views_rank,
+          page_views_rank_delta:        page_views_rank_delta,
+          page_views_per_million:       page_views_per_million,
+          page_views_per_million_delta: page_views_per_million_delta,
+          page_views_per_user:          page_views_per_user,
+          page_views_per_user_delta:    page_views_per_user_delta
+        }
+      end
+    end
+
+    # The site's Alexa rank (a legacy method)
     def rank(url, params = {})
-      result({"ResponseGroup" => "Rank", "Url" => url}, params) do |document|
-        document.at("//UrlInfoResult/Alexa/TrafficData/Rank") &&
-          document.at("//UrlInfoResult/Alexa/TrafficData/Rank").text.to_i
-      end 
+      get(url, params)[:ranking]
     end
 
     private
+
+    attr_accessor :document
 
     def host; "awis.amazonaws.com" end
     def path; "/" end
     def default_params; {"Action" => "UrlInfo"}; end
 
+    def speed_median_load_time
+      document.at("//UrlInfoResult/Alexa/ContentData/Speed/MedianLoadTime") &&
+        document.at("//UrlInfoResult/Alexa/ContentData/Speed/MedianLoadTime").text.to_i
+    end
+
+    def speed_load_percentile
+      document.at("//UrlInfoResult/Alexa/ContentData/Speed/Percentile") &&
+        document.at("//UrlInfoResult/Alexa/ContentData/Speed/Percentile").text.to_i
+    end
+
+    def link_count
+      document.at("//UrlInfoResult/Alexa/ContentData/LinksInCount") &&
+        document.at("//UrlInfoResult/Alexa/ContentData/LinksInCount").text.to_i
+    end
+
+    def ranking
+      document.at("//UrlInfoResult/Alexa/TrafficData/Rank") &&
+        document.at("//UrlInfoResult/Alexa/TrafficData/Rank").text.to_i
+    end
+
+    def ranking_delta
+      document.at("//UrlInfoResult/Alexa/TrafficData/UsageStatistics/UsageStatistic/Rank/Delta") &&
+        document.at("//UrlInfoResult/Alexa/TrafficData/UsageStatistics/UsageStatistic/Rank/Delta").text.to_i
+    end
+
+    def reach_rank
+      document.at("//UrlInfoResult/Alexa/TrafficData/UsageStatistics/UsageStatistic/Reach/Rank/Value") &&
+        document.at("//UrlInfoResult/Alexa/TrafficData/UsageStatistics/UsageStatistic/Reach/Rank/Value").text.to_i
+    end
+
+    def reach_rank_delta
+      document.at("//UrlInfoResult/Alexa/TrafficData/UsageStatistics/UsageStatistic/Reach/Rank/Delta") &&
+        document.at("//UrlInfoResult/Alexa/TrafficData/UsageStatistics/UsageStatistic/Reach/Rank/Delta").text.to_i
+    end
+
+    def reach_per_million
+      document.at("//UrlInfoResult/Alexa/TrafficData/UsageStatistics/UsageStatistic/Reach/PerMillion/Value") &&
+        document.at("//UrlInfoResult/Alexa/TrafficData/UsageStatistics/UsageStatistic/Reach/PerMillion/Value").text.gsub(',','').to_i
+    end
+
+    def reach_per_million_delta
+      document.at("//UrlInfoResult/Alexa/TrafficData/UsageStatistics/UsageStatistic/Reach/PerMillion/Delta") &&
+        document.at("//UrlInfoResult/Alexa/TrafficData/UsageStatistics/UsageStatistic/Reach/PerMillion/Delta").text.to_i
+    end
+
+    def page_views_rank
+      document.at("//UrlInfoResult/Alexa/TrafficData/UsageStatistics/UsageStatistic/PageViews/Rank/Value") &&
+        document.at("//UrlInfoResult/Alexa/TrafficData/UsageStatistics/UsageStatistic/PageViews/Rank/Value").text.to_i
+    end
+
+    def page_views_rank_delta
+      document.at("//UrlInfoResult/Alexa/TrafficData/UsageStatistics/UsageStatistic/PageViews/Rank/Delta") &&
+        document.at("//UrlInfoResult/Alexa/TrafficData/UsageStatistics/UsageStatistic/PageViews/Rank/Delta").text.to_i
+    end
+
+    def page_views_per_million
+      document.at("//UrlInfoResult/Alexa/TrafficData/UsageStatistics/UsageStatistic/PageViews/PerMillion/Value") &&
+        document.at("//UrlInfoResult/Alexa/TrafficData/UsageStatistics/UsageStatistic/PageViews/PerMillion/Value").text.to_i
+    end
+
+    def page_views_per_million_delta
+      document.at("//UrlInfoResult/Alexa/TrafficData/UsageStatistics/UsageStatistic/PageViews/PerMillion/Delta") &&
+        document.at("//UrlInfoResult/Alexa/TrafficData/UsageStatistics/UsageStatistic/PageViews/PerMillion/Delta").text.to_i
+    end
+
+    def page_views_per_user
+      document.at("//UrlInfoResult/Alexa/TrafficData/UsageStatistics/UsageStatistic/PageViews/PerUser/Value") &&
+        document.at("//UrlInfoResult/Alexa/TrafficData/UsageStatistics/UsageStatistic/PageViews/PerUser/Value").text.to_i
+    end
+
+    def page_views_per_user_delta
+      document.at("//UrlInfoResult/Alexa/TrafficData/UsageStatistics/UsageStatistic/PageViews/PerUser/Delta") &&
+        document.at("//UrlInfoResult/Alexa/TrafficData/UsageStatistics/UsageStatistic/PageViews/PerUser/Delta").text.to_i
+    end
   end
 end
--- spec/url_info_spec.rb
+++ spec/url_info_spec.rb
@@ -2,26 +2,118 @@ require_relative "spec_helper"
 
 module Ralexa
   describe UrlInfo do
-
     let(:url_info) { UrlInfo.new(client) }
-    let(:client) { MiniTest::Mock.new }
+    let(:client)   { MiniTest::Mock.new }
 
     describe "#rank" do
       def expected_params
         {
           "Action" => "UrlInfo",
-          "ResponseGroup" => "Rank",
-          "Url" => "google.com"
+          "ResponseGroup" => "Related,TrafficData,ContentData",
+          "Url" => "flippa.com"
         }
       end
+
       before do
         client.expect :get, fixture("rank.xml"),
           ["awis.amazonaws.com", "/", expected_params]
       end
+
       it "fetches, parses, and returns the site's rank" do
-        rank = url_info.rank("google.com")
-        rank.must_equal 1
-      end 
+        rank = url_info.rank("flippa.com")
+        rank.must_equal 932
+      end
+   end
+
+    describe "#info" do
+      def expected_params
+        {
+          "Action"        => "UrlInfo",
+          "ResponseGroup" => "Related,TrafficData,ContentData",
+          "Url"           => "flippa.com"
+        }
+      end
+
+      before do
+        client.expect :get, fixture("rank.xml"),
+          ["awis.amazonaws.com", "/", expected_params]
+      end
+
+      it "returns the alexa speed median load time in the result" do
+        result = url_info.get("flippa.com")[:speed_median_load_time]
+        result.must_equal 1930
+      end
+
+      it "returns the alexa speed load percentile in the result" do
+        result = url_info.get("flippa.com")[:speed_load_percentile]
+        result.must_equal 42
+      end
+
+      it "returns the alexa link count in the result" do
+        result = url_info.get("flippa.com")[:link_count]
+        result.must_equal 1788
+      end
+
+      it "returns the alexa ranking in the result" do
+        result = url_info.get("flippa.com")[:ranking]
+        result.must_equal 932
+      end
+
+      it "returns the alexa ranking delta in the result" do
+        result = url_info.get("flippa.com")[:ranking_delta]
+        result.must_equal -695
+      end
+
+      it "returns the alexa reach rank in the result" do
+        result = url_info.get("flippa.com")[:reach_rank]
+        result.must_equal 1230
+      end
+
+      it "returns the alexa reach rank delta in the result" do
+        result = url_info.get("flippa.com")[:reach_rank_delta]
+        result.must_equal -868
+      end
+
+      it "returns the alexa reach per million in the result" do
+        result = url_info.get("flippa.com")[:reach_per_million]
+        result.must_equal 1093
+      end
+
+      it "returns the alexa reach per million delta in the result" do
+        result = url_info.get("flippa.com")[:reach_per_million_delta]
+        result.must_equal 90
+      end
+
+      it "returns the alexa page views rank in the result" do
+        result = url_info.get("flippa.com")[:page_views_rank]
+        result.must_equal 579
+      end
+
+      it "returns the alexa page views rank delta in the result" do
+        result = url_info.get("flippa.com")[:page_views_rank_delta]
+        result.must_equal -406
+      end
+
+      it "returns the alexa page views per million in the result" do
+        result = url_info.get("flippa.com")[:page_views_per_million]
+        result.must_equal 98
+      end
+
+      it "returns the alexa page views per million delta in the result" do
+        result = url_info.get("flippa.com")[:page_views_per_million_delta]
+        result.must_equal 92
+      end
+
+      it "returns the alexa page views per user in the result" do
+        result = url_info.get("flippa.com")[:page_views_per_user]
+        result.must_equal 9
+      end
+
+      it "returns the alexa page views per user delta in the result" do
+        result = url_info.get("flippa.com")[:page_views_per_user_delta]
+        result.must_equal 1
+      end
+
     end
 
   end
